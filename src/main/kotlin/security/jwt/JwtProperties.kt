3062b31cba688cd6edecab5464cc9c54a7412716
yuhaeni <haeni311@gmail.com>
2026-01-01 13:11:45 +0700

    chore: move jwt util

--- src/main/kotlin/security/jwt/JwtProperties.kt
+++ src/main/kotlin/security/jwt/JwtProperties.kt
@@ -1,4 +1,4 @@
-package com.kou.kouappapi.config
+package security.jwt
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
--- src/main/kotlin/security/jwt/JwtTokenProvider.kt
+++ src/main/kotlin/security/jwt/JwtTokenProvider.kt
@@ -1,4 +1,4 @@
-package com.kou.kouappapi.config
+package security.jwt
 
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.ExpiredJwtException
