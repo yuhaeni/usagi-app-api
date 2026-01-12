f4b73df9bb222dddd2676c11b91f8cd23914723e
yuhaeni <haeni311@gmail.com>
2026-01-12 10:24:33 +0900

    feat: 회원가입 후 프로필 설정(이름, 비밀번호)

--- src/main/kotlin/com/kou/kouappapi/auth/service/dto/CompleteProfileRequestDto.kt
+++ src/main/kotlin/com/kou/kouappapi/auth/service/dto/CompleteProfileRequestDto.kt
@@ -4,5 +4,6 @@ data class CompleteProfileRequestDto(
     val userId: Long,
     val name: String,
     val password: String?,
+    val profileImageUrl: String?,
     val profileComplete: Boolean = true,
 )
