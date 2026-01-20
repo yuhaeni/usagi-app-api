dc1aa7d2408843931caa3ef8cec3c013957c9487
yuhaeni <haeni311@gmail.com>
2026-01-20 17:57:33 +0900

    test: 테스트 컨테이너 설정 수정

--- src/test/kotlin/com/kou/kouappapi/IntegrationTestSupport.kt
+++ src/test/kotlin/com/kou/kouappapi/IntegrationTestSupport.kt
@@ -7,27 +7,21 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
 @SpringBootTest
-@Testcontainers
 abstract class IntegrationTestSupport(
     body: DescribeSpec.() -> Unit = {},
 ) : DescribeSpec(body) {
     override fun extensions(): List<Extension> = listOf(SpringExtension)
 
     companion object {
-        @Container
-        @ServiceConnection
         val redisContainer =
             GenericContainer(DockerImageName.parse("redis:alpine"))
                 .withExposedPorts(6379)
                 .apply { start() }
 
-        @Container
-        @ServiceConnection // Spring이 자동으로 datasource url, username, password를 교체해줌
+        @ServiceConnection
         val postgresContainer =
             PostgreSQLContainer<Nothing>(DockerImageName.parse("postgres:15-alpine"))
                 .apply {
@@ -35,5 +29,10 @@ abstract class IntegrationTestSupport(
                     withUsername("test")
                     withPassword("test")
                 }.apply { start() }
+
+        init {
+            System.setProperty("spring.data.redis.host", redisContainer.host)
+            System.setProperty("spring.data.redis.port", redisContainer.getMappedPort(6379).toString())
+        }
     }
 }
