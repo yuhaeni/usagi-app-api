e7452ae8857e8b282555a2d8b4c7d43aa49d590d
yuhaeni <haeni311@gmail.com>
2026-02-15 18:04:37 +0900

    fix(entity): refresh token 문자열 제한 수정

--- src/main/kotlin/com/kou/kouappapi/entity/RefreshToken.kt
+++ src/main/kotlin/com/kou/kouappapi/entity/RefreshToken.kt
@@ -1,5 +1,6 @@
 package com.kou.kouappapi.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -9,6 +10,7 @@ import java.util.Date
 @Entity
 class RefreshToken(
     val userId: Long,
+    @Column(length = 500)
     val tokenHash: String,
     val expiresAt: Date,
 ) : BaseEntity() {
