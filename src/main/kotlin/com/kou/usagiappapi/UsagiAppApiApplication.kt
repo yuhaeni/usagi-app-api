dfe136805e03da3d0b53bc7fc63d9157983e3859
yuhaeni <haeni311@gmail.com>
2026-03-10 21:26:32 +0900

    refactor: 메인 클래스명 변경

--- src/main/kotlin/com/kou/usagiappapi/UsagiAppApiApplication.kt
+++ src/main/kotlin/com/kou/usagiappapi/UsagiAppApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KouAppApiApplication
+class UsagiAppApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<KouAppApiApplication>(*args)
+    runApplication<UsagiAppApiApplication>(*args)
 }
